be181a640268948b52689c9f84d0f422b2f3d434
Brandt Strom <brandtkstrom@gmail.com>
2019-10-11 03:49:56 -0700

    Updates

---
+++ precache-manifest.fb5c5eec398e5ead30e258b2e1065a0e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "896a4b5617464a1f1d4c98c1f7af7e29",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "f4e959965334487fd71b",
+    "url": "/clicky-game/static/js/2.b03ded27.chunk.js"
+  },
+  {
+    "revision": "64e03f682e834db95115",
+    "url": "/clicky-game/static/js/main.acbf214c.chunk.js"
+  },
+  {
+    "revision": "61fe689653f34435d57f",
+    "url": "/clicky-game/static/js/runtime-main.8c94c7f2.js"
+  }
+]);
